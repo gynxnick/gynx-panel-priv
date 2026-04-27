e08845f2fd6bcc9b3acbda6dad525c6c3d339417
gynxnick <nick@gynx.gg>
2026-04-26 21:40:40 -0400

    Revert "feat(licensing): admin-managed license keys (Option B)"
    This reverts commit be041efcc9a481f69ded9e1ea13c9dd40337e4f9.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -152,11 +152,6 @@
                                 <i class="fa fa-globe"></i> <span>Subdomains</span>
                             </a>
                         </li>
-                        <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.licenses') ?: 'active' }}">
-                            <a href="{{ route('admin.licenses.index') }}">
-                                <i class="fa fa-key"></i> <span>License Keys</span>
-                            </a>
-                        </li>
                     </ul>
                 </section>
             </aside>
